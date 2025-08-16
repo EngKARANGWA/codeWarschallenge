3de0c03577a53442968e7dc462ac9599ca0b4b1c
KARANGWA <engineerkarangwa@gmail.com>
2025-08-16 23:39:37 +0200

    8kt challenge sakurafall

---
+++ sakurafall.js
@@ -0,0 +1,12 @@
+function Sakurafall(v) {
+    let g = 9.8; // acceleration due to gravity in m/s^2
+    let speed=5;
+    let time=80;
+if(v<=0){
+return 0;
+}
+else{
+    let distance = (speed * time) ;
+}
+    return distance/v;
+}
